f4b63c80d1f4cb991e92c5edd9cdfd6402d9b723
Leon Bichmann <bichmann@informatik.uni-tuebingen.de>
2018-11-21 12:43:24 +0100

    add coverage csv

--- hotspots.R
+++ hotspots.R
@@ -141,19 +141,24 @@ for (protid in unique(df$Single.Proteins)){
   df_out_hot<-df_out[which(df_out$Sequence %in% unlist(hotspot_pep)),]
   if (exclusive_switch){
     capture.output(print(hotspot_pep), file = paste0(outdir,"/tumor_associated/",protid,"_hotspots_matches.csv"))
+    write.csv(sums, file = paste0(outdir,"/tumor_associated/",protid,"_coverage.csv"))
     write.csv(df_out_hot, file = paste0(outdir,"/tumor_associated/",protid,"_hotspot_peptides_only.csv"))
     write.csv(df_out, file = paste0(outdir,"/tumor_associated/",protid,"_all_peptides.csv"))
     ggsave(plot = p, filename = paste0(outdir,"/tumor_associated/",protid,"_hotspots.png"))
   } else if ((benign_c<=n_hit_wonder) & (malign_c<=n_hit_wonder)){
+    write.csv(sums, file = paste0(outdir,"/n_hit_wonders/",protid,"_coverage.csv"))
     write.csv(df_out, file = paste0(outdir,"/n_hit_wonders/",protid,"_all_peptides.csv"))
     ggsave(plot = p, filename = paste0(outdir,"/n_hit_wonders/",protid,"_hotspots.png"))    
   } else if (benign_c==0){
+    write.csv(sums, file = paste0(outdir,"/only_malign/",protid,"_coverage.csv"))
     write.csv(df_out, file = paste0(outdir,"/only_malign/",protid,"_all_peptides.csv"))
     ggsave(plot = p, filename = paste0(outdir,"/only_malign/",protid,"_hotspots.png")) 
   } else if (malign_c==0){
+    write.csv(sums, file = paste0(outdir,"/only_benign/",protid,"_coverage.csv"))
     write.csv(df_out, file = paste0(outdir,"/only_benign/",protid,"_all_peptides.csv"))
     ggsave(plot = p, filename = paste0(outdir,"/only_benign/",protid,"_hotspots.png")) 
   } else {
+    write.csv(sums, file = paste0(outdir,"/all/",protid,"_coverage.csv"))
     write.csv(df_out, file = paste0(outdir,"/all/",protid,"_peptides.csv") )
     ggsave(plot = p, filename = paste0(outdir,"/all/",protid,"_hotspots.png"))
   }
